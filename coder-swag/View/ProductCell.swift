52dd438d740b26169c68a7100ee36500d164462a
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-16 08:52:07 -0500

    Create ProductCell class and connect IBoutlets

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  coder-swag
+//
+//  Created by Eric Masiello on 1/16/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
