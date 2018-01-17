2918cae52ec0c9d9a784d81748969c26430fb885
Eric Masiello <eric.j.masiello@gmail.com>
2018-01-17 18:39:15 -0500

    Add stub for ProductsVC, associate the Product view to the ProductsVC class and name the segue ProductsVC

---
+++ coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,19 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Eric Masiello on 1/17/18.
+//  Copyright © 2018 Eric Masiello. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
